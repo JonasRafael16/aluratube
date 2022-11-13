fc6691ca6a8829c1126c9e1d5f15fdad47ae3a7c
Jonas Rafael de Lima <jonasrafael16@gmail.com>
2022-11-13 20:42:17 -0300

    feat: create supabase conection

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyledRegisterVideo } from "./styles";
+import { createClient } from "@supabase/supabase-js";
+import { RiVideoAddFill as AddIcon } from 'react-icons/ri';
+
 
 function useForm(formProps) {
   const [videoValues, setVideoValues] = React.useState(formProps.initialValues)
@@ -20,26 +23,48 @@ function useForm(formProps) {
   };
 }
 
+const PROJECT_URL = "https://bagorjfquyvidaaxqswy.supabase.co";
+const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhZ29yamZxdXl2aWRhYXhxc3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxNjYwMDcsImV4cCI6MTk4Mzc0MjAwN30.ctwgbHyJyp44OZ44IBLJWPajmI8z8B2G6rBJ1QwCrKM";
+const supabase = createClient(PROJECT_URL, PUBLIC_KEY)
+
+function getUrlThum(videoUrl) {
+  return `https://img.youtube.com/vi/${videoUrl.split("v=")[1]}/hqdefault.jpg`;
+}
+
 export default function RegisterVideo() {
   const registrationForm = useForm({
-    initialValues: { title: "teste", url: "https://you..." }
+    initialValues: {}
   });
   const [visibleForm, setVisibleForm] = React.useState(false);
 
   return (
     <StyledRegisterVideo>
       <button className="add-video" onClick={() => setVisibleForm(true)}>
-        {/* <AddIcon className="position-icon" /> */}
+        <AddIcon className="position-icon" />
       </button>
 
       {visibleForm
         && (
           <form onSubmit={(event) => {
             event.preventDefault();
+
+            supabase.from("videos").insert({
+              title: registrationForm.videoValues.title,
+              url: registrationForm.videoValues.url,
+              thumb: getUrlThum(registrationForm.videoValues.url),
+              playlist: registrationForm.videoValues.playlist,
+            })
+              .then((result) => {
+                alert("Video Inserido!")
+
+              })
+              .catch((error) => {
+                console.log(error);
+              })
+
             setVisibleForm(false);
             registrationForm.clearForm()
           }}>
-
             <div>
               <button
                 type="button"
